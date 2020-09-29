9985781cd4daa74bf2296595f107b71300a2facf
Zach <zachery.goodrich@gmail.com>
2020-09-29 15:37:39 -0700

    location should pull need to test

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -1,10 +1,46 @@
 const { ipMiddleware, getIpLocation } = require('../services/clientIPservices');
+const https = require('https');
 
 const getClientIpLocation = async (req, res) => {
-    let userIp = ipMiddleware(req, res);
-    let location = await getIpLocation(userIp);
-    res.json(location);
-    console.log(location)
+    let userIp = await ipMiddleware(req, res);
+    let ipLocation = ''
+
+    const options = {
+        host: 'ipvigilante.com',
+        path: `/${userIp}/full`,
+        port: 443,
+        method: 'GET',
+        headers: {'User-Agent' : 'request'}
+    };
+
+    https.get(options, function (res) {
+        let json = '';
+        res.on('data', function (chunk) {
+            json += chunk;
+        });
+
+        res.on('end', function () {
+            if (res.statusCode === 200) {
+                try {
+                    let result = JSON.parse(json);
+                    ipLocation = result.data.country_name
+                    console.log(ipLocation);
+
+                } catch (e) {
+                    console.log('Error');
+                }
+            } else {
+                console.log(`Status: ${res.statusCode}`)
+            }
+        });
+    }).on('error', function (err) {
+        console.log(err);
+    })
+
+
+    // let location = getIpLocation(options);
+    // // res.json(location);
+    // console.log(location)
     res.json(userIp);
 }
 
--- services/clientIPservices.js
+++ services/clientIPservices.js
@@ -1,23 +1,38 @@
+'use strict';
 const requestIp = require('request-ip');
 const https = require('https');
 
-const ipMiddleware = (req, res) => {
-    const clientIp = requestIp.getClientIp(req);
-    return clientIp;
+const ipMiddleware = async (req, res) => {
+    return requestIp.getClientIp(req);
 }
 
 const API_URL = 'https://ipvigilante.com/json';
 
-const getIpLocation = (ip, res) => {
-    const options = {
-        host: 'ipvigilante.com',
-        path: `${ip}/full`,
-        port: 443,
-        method: 'GET',
-        headers: {'User-Agent' : 'request'}
-    };
+const getIpLocation = (options) => {
 
-    https.get(options, (res) => {})
+    https.get(options, function (res) {
+        let json = '';
+        res.on('data', function (chunk) {
+            json += chunk;
+        });
+
+        res.on('end', function () {
+            if (res.statusCode === 200) {
+                try {
+                    let result = JSON.parse(json);
+                    return result
+                    console.log(result);
+
+                } catch (e) {
+                    console.log('Error');
+                }
+            } else {
+                console.log(`Status: ${res.statusCode}`)
+            }
+        });
+    }).on('error', function (err) {
+        console.log(err);
+    })
 }
 
 module.exports = {
