bac8103657129265a3ce8bb6177f94fa707cd524
Zach <zachery.goodrich@gmail.com>
2020-09-29 14:14:08 -0700

    sending location

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -3,6 +3,7 @@ const { ipMiddleware, getIpLocation } = require('../services/clientIPservices');
 const getClientIpLocation = async (req, res) => {
     let userIp = ipMiddleware(req, res);
     let location = await getIpLocation(userIp);
+    res.json(location);
     console.log(location)
     res.json(userIp);
 }
