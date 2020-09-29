21d95fd72d0688acde3b30b912a0a806cb50490d
Zach <zachery.goodrich@gmail.com>
2020-09-28 23:00:51 -0700

    sending to client

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -2,10 +2,11 @@ const { ipMiddleware, getIpLocation } = require('../services/clientIPservices');
 
 const getClientIpLocation = async (req, res) => {
     let userIp = ipMiddleware(req, res);
-    let ipLocation = await getIpLocation(userIp);
-    console.log(ipLocation);
+    // let ipLocation = await getIpLocation(userIp);
+    // console.log(ipLocation);
 
     res.send(`You are currently in ${userIp}`);
+    res.json(userIp);
 }
 
 module.exports = {
