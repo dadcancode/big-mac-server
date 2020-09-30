5cb586532c2ea8b902b868af7bee2b87c8ffbee2
Zach <zachery.goodrich@gmail.com>
2020-09-29 18:18:08 -0700

    imported csv file

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
-
+const csvFile = require('../repositories/mac-data.csv');
 
 
 
 const getMacData = () => {
-    fs.readFile('../repositories/mac-data.csv', 'utf8', function (err, data) {
+    fs.readFile(csvFile, function (err, data) {
         if (err) throw err;
         console.log(data);
     })
