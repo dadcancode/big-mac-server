19ae725cf8f2587813416f1e1f8ac8e56c773768
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:44:25 -0700

    err throw

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 
 const getMacData = () => {
     let result = fs.readFile('file:///C:/Users/Crystal/Desktop/Portfolio/big-mac-server/staticData/mac-data.txt', (err, data) => {
+
+        if(err) {
+            throw err;
+        }
         return data
     })
     return result
