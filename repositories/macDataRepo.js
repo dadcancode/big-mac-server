c06089406934ee898f6942caba0c9951e88c33ad
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:30:25 -0700

    throw err

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const getMacData = async () => {
     let result = await fs.readFile('../staticData/mac-data.txt', (err, data) => {
+        if (err) throw err;
         return data
     })
     return result
