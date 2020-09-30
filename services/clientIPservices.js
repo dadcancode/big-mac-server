c3567b5a72ff5e8b60f18b0b1500c96507562cb7
Zach <zachery.goodrich@gmail.com>
2020-09-29 20:42:02 -0700

    fixed git

--- services/clientIPservices.js
+++ services/clientIPservices.js
@@ -3,53 +3,59 @@ const requestIp = require('request-ip');
 const https = require('https');
 const getMacData = require('../repositories/macDataRepo');
 
-const ipMiddleware = async (req, res) => {
+const ipMiddleware = async(req, res) => {
     return requestIp.getClientIp(req);
 }
 
 const API_URL = 'https://ipvigilante.com/json';
 
-const getIpLocation = (userIp) => {
+const getIpLocation = async (ip) => {
+    const options = {
+        host: 'ipvigilante.com',
+        path: `${ip}/full`,
+        port: 443,
+        method: 'GET',
+        headers: {'User-Agent' : 'request'}
+    };
+    
+    
+    https.get(options, (res) => {
+        let json = '';
+        res.on('data', (chunk) => {
+            json += chunk;
+        });
 
-    return new Promise((resolve, reject) => {
-        const options = {
-            host: 'ipvigilante.com',
-            path: `/${userIp}/full`,
-            port: 443,
-            method: 'GET',
-            headers: {'User-Agent' : 'request'}
-        };
-    
-        https.get(options, function (res) {
-            let json = '';
-            let result = null;
-            res.on('data', function (chunk) {
-                json += chunk;
-            });
-    
-            res.on('end', function () {
-                if (res.statusCode === 200) {
-                    try {
-                        result = JSON.parse(json);
-                        console.log(result);
-    
-                    } catch (e) {
-                        console.log('Error');
-                    }
-                } else {
-                    console.log(`Status: ${res.statusCode}`)
+        res.on('end', () => {
+            if (res.statusCode === 200) {
+                try {
+                    console.log('i tried!')
+                    let result = JSON.parse(json);
+                    console.log(result)
+                    return result;
+                } catch (e) {
+                    console.log('Error')
                 }
-                resolve(result.data.country_name);
-            });
-        }).on('error', function (err) {
-            console.log(err);
-        })
-    } )
-
+            } else {
+                console.log(`Status ${res.statusCode}`)
+            }
+        });
+    }).on('error', (err) => {
+        console.log(err)
+    });
 }
 
-const parseMacData = () => {
-    getMacData()
+const parseMacData = async () => {
+    let jsonArr = getMacData();
+    let macObj = {};
+
+    let temp = await Promise.all(
+        jsonArr.map((val) => {
+            if(new Date(val['Date']) >= new Date('2016-01-01')) {
+                macObj[val['Country']] = val
+            }
+        })
+    ) 
+    return macObj
 }
 
 module.exports = {
