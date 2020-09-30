a0b74c8253785f05ad28a654eb77c5a26a107ba6
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:22:43 -0700

    no tostring

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const getMacData = async () => {
     fs.readFile('../staticData/mac-data.txt', (err, data) => {
-        console.log(data.toString());
+        console.log(data);
     })
 }
 
