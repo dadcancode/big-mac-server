1cf333cccde95ea579d742824cb0fed05f63c6ea
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:40:46 -0700

    no await

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 
-const getMacData = async () => {
-    let result = await fs.readFile('C:/Users/Crystal/Desktop/Portfolio/big-mac-server/staticData/mac-data.txt', (err, data) => {
+const getMacData = () => {
+    let result = fs.readFile('C:/Users/Crystal/Desktop/Portfolio/big-mac-server/staticData/mac-data.txt', (err, data) => {
         return data
     })
     return result
