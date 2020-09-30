72f295f417fe7db29d2f2e1beacd917fbbb4e0c2
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:34:33 -0700

    using absolute path

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
+
 const getMacData = async () => {
-    let result = await fs.readFile('../staticData/mac-data.txt', (err, data) => {
+    let result = await fs.readFile('C:/Users/Crystal/Desktop/Portfolio/big-mac-server/staticData/mac-data.txt', (err, data) => {
         if (err) throw err;
         return data
     })
