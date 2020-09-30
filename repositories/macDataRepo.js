883b129adbf6469730f13840e36003d6dee3dd92
Zach <zachery.goodrich@gmail.com>
2020-09-29 17:25:59 -0700

    return getmacdata

--- repositories/macDataRepo.js
+++ repositories/macDataRepo.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 
 const getMacData = async () => {
+    let result = null;
     fs.readFile('../staticData/mac-data.txt', (err, data) => {
-        console.log(data);
+        result = data;
     })
+    return result
 }
 
 module.exports = getMacData;
