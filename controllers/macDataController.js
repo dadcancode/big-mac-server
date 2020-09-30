6c4445842df319821701d7a1f6046bc8c9a846d8
Zach <zachery.goodrich@gmail.com>
2020-09-29 22:44:56 -0700

    added mac data controllers

--- controllers/macDataController.js
+++ controllers/macDataController.js
@@ -1,3 +1,16 @@
-const macDataController = () => {
-    
+const { parseMacData } = require('../services/clientIPservices');
+
+
+const getMacDataObj = async (req, res) => {
+    let macData = await parseMacData();
+
+    res.json(macData);
 }
+
+const getCountryMacData = async (req, res) => {
+    let macData = await parseMacData();
+
+    res.json(macData[req.params.country]);
+}
+
+module.exports = { getMacDataObj, getCountryMacData };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
 const { mainController } = require('../controllers/ipController');
+const { getCountryMacData } = require('../controllers/macDataController');
+const getMacData = require('../repositories/macDataRepo');
 
 const router = express.Router();
 
 router.get('/', mainController);
+
+router.get('/macData', getMacData);
+
+router.get('/macData/:country', getCountryMacData);
 
 module.exports = router;
